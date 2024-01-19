a21b06cf992900cfb1411f41b7a6ace7be13c2f3
jezes70 <jezes70@gmail.com>
2024-01-19 14:28:54 +0100

    update

---
+++ app/api/graphql/route.ts
@@ -0,0 +1,27 @@
+import { ApolloServer } from "@apollo/server";
+import { startServerAndCreateNextHandler } from "@as-integrations/next";
+import prisma from "../../../prisma/client";
+import { NextRequest, NextResponse } from "next/server";
+import { typeDefs } from "./typedefs";
+import { resolvers } from "./resolver";
+
+const server = new ApolloServer({
+  resolvers,
+  typeDefs,
+});
+
+const handler = startServerAndCreateNextHandler<NextRequest>(server, {
+  context: async (req, res) => ({
+    req,
+    res,
+    dataSources: prisma,
+  }),
+});
+
+export async function GET(request: NextRequest) {
+  return handler(request);
+}
+
+export async function POST(request: NextRequest) {
+  return handler(request);
+}
